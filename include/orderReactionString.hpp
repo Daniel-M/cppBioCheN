6845494ebbd92938dfdeed5395cb7929d1836a7c
Daniel-M <danielmejia55@gmail.com>
2014-02-27 15:04:54 -0500

    This compiles, works but doesnt take into account the case '->'

--- include/orderReactionString.hpp
+++ include/orderReactionString.hpp
@@ -15,88 +15,46 @@
 void orderReactionString(std::string sInput,std::vector<std::string>& vsOutput)
 {
   
-	//std::cout << sInput << std::endl;
+  std::cout << sInput << std::endl;
   
   std::vector< std::string > vsPatterns;
   
   vsPatterns.push_back("<->");
   vsPatterns.push_back("<=>");
-  vsPatterns.push_back("->");
-
+    
   std::vector<std::string>* vsBuffer = new std::vector<std::string>;
-
-  //std::cout << "subs " <<  sInput.substr(sInput.find(vsPatterns[2])-vsPatterns[2].length()+1,1) << std::endl;
-  //std::vector<std::string>::iterator vit=vsOutput.end();
+  
  
   for(int i=0;i!=vsPatterns.size();i++)
   {
+    
+    while(sInput.find(vsPatterns[i])<sInput.length())
+    {
+      
+    /* The left side of sInput */
+    std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
+      
+    /* The rigth side of sInput */
+    std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
  
-	std::cout << vsPatterns[i] << std::endl;
-
-	if(vsPatterns[i] == "->" && sInput.substr(sInput.find("->"),1) != "<" ) 
-		//sInput.substr(sInput.find(vsPatterns[i])-vsPatterns[i].length()+1,1) != "<" ) 
-	{
-			
-		std::cout << sInput << std::endl;
-		
-		/* The left side of sInput */
-		std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
-			  
-		/* The rigth side of sInput */
-		std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
-		
-		vsOutput.push_back(*sLTemp+"->"+*sRTemp);
-
-		//std::cout << vit << std::endl;
-		std::cout << "by now " << vsOutput.back() << std::endl;
-		
-		delete(sRTemp);
-		delete(sLTemp);
-
-	}
-	else
-	{
-		std::cout << sInput << std::endl;
-		
-		while(sInput.find(vsPatterns[i]) < sInput.length())
-		{
-		  
-			/* The left side of sInput */
-			std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
-			  
-			/* The rigth side of sInput */
-			std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
-		 
-			/* Store the part of string into the vector */
-			vsBuffer->push_back(*sLTemp);
-			
-			sInput=*sRTemp;
-			
-			std::cout << " ... " << sInput << std::endl;
-			
-			if(sRTemp->find(vsPatterns[i])>sRTemp->length())
-			{
-			  vsBuffer->push_back(*sRTemp);
-			}
-			
-			delete(sRTemp);
-			delete(sLTemp);
-		}
-
-		std::cout << " debug " << std::endl;
-
-		for(int i=0;i<vsBuffer->size();i++)
-		{
-			std::cout << "aa " << i << " - " << vsBuffer->at(i) << std::endl;
-		}
-
-		for(int j=0;j<vsBuffer->size()-1;j++)
-		{
-			vsOutput.push_back(vsBuffer->at(j)+"->"+vsBuffer->at(j+1));
-			vsOutput.push_back(vsBuffer->at(j+1)+"->"+vsBuffer->at(j));
-		}
-	}
+    /* Store the part of string into the vector */
+    vsBuffer->push_back(*sLTemp);
+    
+    sInput=*sRTemp;
+    
+    if(sRTemp->find(vsPatterns[i])>sRTemp->length())
+    {
+      vsBuffer->push_back(*sRTemp);
+    }
+    
+    delete(sRTemp);
+    delete(sLTemp);
+    }
   }
   
-  
+  for(int i=0;i<vsBuffer->size()-1;i++)
+  {
+    vsOutput.push_back(vsBuffer->at(i)+"->"+vsBuffer->at(i+1));
+    vsOutput.push_back(vsBuffer->at(i+1)+"->"+vsBuffer->at(i));
+  }
 }
