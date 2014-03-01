456c9e9e2023da834105aff60728c9f761f01e7d
Daniel-M <danielmejia55@gmail.com>
2014-02-28 23:30:24 -0500

    Commit before changing the algorithm for orderReactionStrings.hpp

--- include/orderReactionString.hpp
+++ include/orderReactionString.hpp
@@ -14,49 +14,85 @@
 
 void orderReactionString(std::string sInput,std::vector<std::string>& vsOutput)
 {
-  
-  std::cout << sInput << std::endl;
-  
-  std::vector< std::string > vsPatterns;
-  
-  vsPatterns.push_back("<->");
-  vsPatterns.push_back("<=>");
-  vsPatterns.push_back("->");
+
+	std::cout << sInput << std::endl;
+
+	std::vector< std::string > vsPatterns;
+
+	vsPatterns.push_back("<->");
+	vsPatterns.push_back("<=>");
+	vsPatterns.push_back("->");
 
 
-  std::vector<std::string>* vsBuffer = new std::vector<std::string>;
-  
- 
-  for(int i=0;i!=vsPatterns.size();i++)
-  {
-    
-    while(sInput.find(vsPatterns[i])<sInput.length())
-    {
-      
-    /* The left side of sInput */
-    std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
-      
-    /* The rigth side of sInput */
-    std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
- 
-    /* Store the part of string into the vector */
-    vsBuffer->push_back(*sLTemp);
-    
-    sInput=*sRTemp;
-    
-    if(sRTemp->find(vsPatterns[i])>sRTemp->length())
-    {
-      vsBuffer->push_back(*sRTemp);
-    }
-    
-    delete(sRTemp);
-    delete(sLTemp);
-    }
-  }
-  
-  for(int i=0;i<vsBuffer->size()-1;i++)
-  {
-    vsOutput.push_back(vsBuffer->at(i)+"->"+vsBuffer->at(i+1));
-    vsOutput.push_back(vsBuffer->at(i+1)+"->"+vsBuffer->at(i));
-  }
-}
+	std::vector<std::string>* vsBuffer = new std::vector<std::string>;
+
+
+	for(int i=0;i!=vsPatterns.size();i++)
+	{
+		if(vsPatterns[i]=="->")
+		{
+			if( sInput.substr(sInput.find(vsPatterns[i])-1,sInput.find(vsPatterns[i])) != "<" )
+			{
+				while(sInput.find(vsPatterns[i])<sInput.length())
+				{
+
+					/* The left side of sInput */
+					std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
+
+					/* The rigth side of sInput */
+					std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
+
+					/* Store the part of string into the vector */
+					vsBuffer->push_back(*sLTemp);
+
+					sInput=*sRTemp;
+
+					if(sRTemp->find(vsPatterns[i])>sRTemp->length())
+					{
+						vsBuffer->push_back(*sRTemp);
+					}
+
+					delete(sRTemp);
+					delete(sLTemp);
+				}
+
+				for(int i=0;i<vsBuffer->size()-1;i++)
+				{
+					vsOutput.push_back(vsBuffer->at(i)+"->"+vsBuffer->at(i+1));
+				}
+			}
+		}
+		else
+		{
+			while(sInput.find(vsPatterns[i])<sInput.length())
+			{
+
+				/* The left side of sInput */
+				std::string* sLTemp = new std::string(sInput.substr(0,sInput.find(vsPatterns[i])));
+
+				/* The rigth side of sInput */
+				std::string* sRTemp = new std::string(sInput.substr(sInput.find(vsPatterns[i])+vsPatterns[i].length()));
+
+				/* Store the part of string into the vector */
+				vsBuffer->push_back(*sLTemp);
+
+				sInput=*sRTemp;
+
+				if(sRTemp->find(vsPatterns[i])>sRTemp->length())
+				{
+					vsBuffer->push_back(*sRTemp);
+				}
+
+				delete(sRTemp);
+				delete(sLTemp);
+			}
+
+			for(int i=0;i<vsBuffer->size()-1;i++)
+			{
+				vsOutput.push_back(vsBuffer->at(i)+"->"+vsBuffer->at(i+1));
+				vsOutput.push_back(vsBuffer->at(i+1)+"->"+vsBuffer->at(i));
+			}
+		}
+	}
+}   
+
